c634c5ad621bbd90ad07478bc6fe240c5fc19333
shadyrajab <shadyrajaab@gmail.com>
2023-08-10 16:18:42 -0300

    First commit

--- dashboard.py
+++ dashboard.py
@@ -1,17 +1,29 @@
+'''Importando as bibliotecas'''
+
 import streamlit as st
 from datapov.calendar import calendario
+
+
+'''Criando as opções de filtro'''
+
 
 filter_options = ['Nenhum', 'Status', 'Forma de ingresso']
 status_options = ['Nenhum', 'Inscrições Encerradas', 'Inscrições Abertas', 'Inscrições Fechadas']
 form_options = ['Nenhum', 'Vestibular', 'Nota ENEM', 'Vestibular/ Nota ENEM']
 filter_selected, status_selected, form_selected = '', '', ''
 
+
+'''Configurando o design inicial da página'''
 st.title('Calendário Apoio Singular')
 st.write('----------')
 st.write('#### Filtros')
 
+
+'''Criando caixa de seleção para o filtro'''
 filter_selected = st.selectbox('Filtrar por:', options=filter_options)
 
+
+'''Lendo o filtro escolhido pelo usuário e adaptando o calendário ao filtro'''
 if filter_selected != 'Nenhum': 
     calendario = calendario
     if filter_selected == 'Status':
@@ -33,4 +45,6 @@ if filter_selected != 'Nenhum':
             case 'Vestibular/ Nota ENEM':
                 calendario = calendario[calendario['Forma de ingresso'] == 'Vestibular/ Nota ENEM']
 
+
+''' Plotando o calendário '''
 calend = st.dataframe(calendario)
--- datapov/calendar.py
+++ datapov/calendar.py
@@ -1,6 +1,9 @@
 import pandas as pd
 
+'''Transformando a base de dados em um dataframe'''
 calendario = pd.read_excel('calendario.xlsx').set_index('Processo Seletivo')
+
+'''Formatando as datas do calendário'''
 calendario['Fim'] = calendario['Fim'].dt.strftime('%d/%m/%Y')
 calendario['Início'] = calendario['Início'].dt.strftime('%d/%m/%Y')
 calendario['Data Final de Pagamento'] = calendario['Data Final de Pagamento'].dt.strftime('%d/%m/%Y')
@@ -10,5 +13,7 @@ calendario['Data da prova discursiva'] = calendario['Data da prova discursiva'].
 calendario['Edital'] = calendario['Edital'].dt.strftime('%d/%m/%Y')
 calendario['Resultado'] = calendario['Resultado'].dt.strftime('%d/%m/%Y')
 calendario['Divulgação gabarito'] = calendario['Divulgação gabarito'].dt.strftime('%d/%m/%Y')
+
+'''Preenchendo os valores nulos como Não informado'''
 calendario = calendario.fillna('Não informado')
 
